eaf526f640389f961d84ea17bff6d0849fb2288b
Vini <marcosvhd1@gmail.com>
2025-02-19 23:43:51 -0300

    home screen layout fix

--- lib/modules/home/screens/home_screen.dart
+++ lib/modules/home/screens/home_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:geapp/app/components/layout.dart';
-import 'package:geapp/app/components/logo.dart';
 import 'package:geapp/modules/home/components/nav_button.dart';
+import 'package:geapp/modules/home/components/user_avatar.dart';
 import 'package:geapp/routes/routes.dart';
 import 'package:geapp/themes/extension.dart';
-import 'package:geapp/themes/text.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,24 +13,7 @@ class HomeScreen extends StatelessWidget {
     return Layout(
       withDrawer: true,
       backButton: false,
-      title: Padding(
-        padding: const EdgeInsets.only(left: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            LogoGEApp(),
-            const SizedBox(width: 12),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Bem-vindo,", style: TText.md),
-                const SizedBox(height: 5),
-                Text("Usuário", style: TText.ml),
-              ],
-            ),
-          ],
-        ),
-      ),
+      title: UserAvatar(),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
